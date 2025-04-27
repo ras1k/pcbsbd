9396566ddfcfe4b887880cff3fff4f1ebc054a4f
Rasik Abdullah <rasikabdullah@gmail.com>
2025-04-27 22:05:41 +0600

    update

--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -2,14 +2,14 @@
     <div class="min-h-screen flex flex-col">
       <Navbar />
       <main class="flex-1">
-        <slot />  <!-- This will render content from the page (e.g., index.vue) -->
+        <slot /> 
       </main>
       <Footer />
     </div>
   </template>
   
   <script setup>
-  import Navbar from '@/components/Navbar.vue'
-  import Footer from '@/components/Footer.vue'
+  import Navbar from '../components/Navbar.vue'
+  import Footer from '../components/Footer.vue'
   </script>
   
